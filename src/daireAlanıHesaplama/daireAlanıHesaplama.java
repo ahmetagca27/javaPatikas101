17a1ef0cdc11d6ec1713d3e7a731b901c63fb589
ahmet.agca <ahmet.agca@yahoo.com>
2023-05-17 23:01:46 +0300

    daireAlanıHesaplama.java

--- src/daireAlanıHesaplama/daireAlanıHesaplama.java
+++ src/daireAlanıHesaplama/daireAlanıHesaplama.java
@@ -2,17 +2,22 @@ package daireAlanıHesaplama;
 import java.util.Scanner;
 public class daireAlanıHesaplama {
     public static void main(String [] args){
-        int r;
+        int r,a;
         double pi=3.14;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Lütfen Dairenin Yarı Çapını Giriniz :");
+        System.out.print("Lütfen Dairenin Yarı Çapını Giriniz : ");
         r = input.nextInt();
+        System.out.print("Lütfen Merkez Açısını Giriniz : ");
+        a = input.nextInt();
 
         double alan = pi * r * r ;
         double cevre = 2 * pi * r;
+        double dairedilimi = ((alan * a) / 360);
 
         System.out.println("Dairenizin Alanı : " + alan);
         System.out.println("Dairenizin Çevresi : " + cevre);
+        System.out.println("Dairenizin Dilimi Alanı : " + dairedilimi);
+
     }
 }
