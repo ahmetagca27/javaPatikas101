42c26feedd47d84cf1926980b1cbe5ba5357c595
ahmet.agca <ahmet.agca@yahoo.com>
2023-05-17 16:17:13 +0300

    ucgenAlanHesaplama

---
+++ src/ucgenAlanHesaplama/UcgenAlanHesaplama.java
@@ -0,0 +1,19 @@
+package ucgenAlanHesaplama;
+import java.util.Scanner;
+public class UcgenAlanHesaplama {
+    public static void main(String[] args) {
+        int a, b;
+        double c;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("1. Kenarı Giriniz : ");
+        a = inp.nextInt();
+        System.out.print("2. Kenarı Giriniz : ");
+        b = inp.nextInt();
+
+        c=Math.sqrt((a*a)+(b*b));
+        System.out.println("Hipotenüs = " + c);
+
+    }
+}
