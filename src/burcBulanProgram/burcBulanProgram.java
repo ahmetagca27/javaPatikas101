ddb04691933b9002ba88c804a989fcf0da327758
ahmet.agca <ahmet.agca@yahoo.com>
2023-05-21 06:17:05 +0300

    burcBulanProgram.java

--- src/burcBulanProgram/burcBulanProgram.java
+++ src/burcBulanProgram/burcBulanProgram.java
@@ -12,7 +12,7 @@ public class burcBulanProgram {
         System.out.print("Doğduğunuz Gün .: ");
         day = input.nextInt();
 
-        switch (month) {
+        /* switch (month) {
             case 1:
                 if (day >=1 && day <= 31) {
                     if (day < 22) {
@@ -147,11 +147,162 @@ public class burcBulanProgram {
                 break;
             default:
                 isError = true;
+        } */
+        if(1<=month && month<=12) {
+            if(month == 1) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 21) {
+                        burc = "Oğlak";
+                    } else {
+                        burc = "Kova";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 2) {
+                if (1 <= month && month <= 28) {
+                    if (day <= 19) {
+                        burc = "Kova";
+                    } else {
+                        burc = "Balık";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 3) {
+                if (1 <= month && month <= 31) {
+                    if (day <= 20) {
+                        burc = "Balık";
+                    } else {
+                        burc = "Koç";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 4) {
+                if (1 <= day && day <= 30) {
+                    if (day <= 20) {
+                        burc = "Koç";
+                    } else {
+                        burc = "Boğa";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 5) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 20) {
+                        burc = "Boğa";
+                    } else {
+                        burc = "İkizler";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 6) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 22) {
+                        burc = "İkizler";
+                    } else {
+                        burc = "Yengeç";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 7) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 22) {
+                        burc = "Yengeç";
+                    } else {
+                        burc = "Aslan";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 8) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 22) {
+                        burc = "Aslan";
+                    } else {
+                        burc = "Başak";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 9) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 22) {
+                        burc = "Başak";
+                    } else {
+                        burc = "Terazi";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 10) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 21) {
+                        burc = "Terazi";
+                    } else {
+                        burc = "Akrep";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else if(month == 11) {
+                if (1 <= day && day <= 31) {
+                    if (day <= 21) {
+                        burc = "Akrep";
+                    } else {
+                        burc = "Yay";
+                    }
+                }
+                else{
+                    isError = true;
+                }
+            }
+            else {
+                if (1 <= day && day <= 31) {
+                    if (day <= 21) {
+                        burc = "Yay";
+                    } else {
+                        burc = "Oğlak";
+                    }
+                }else{
+                    isError = true;
+                }
+            }
+
         }
-        if (isError){
-            System.out.println("Hatalı bir Giriş Yaptınız, Lütfen Tekrar deneyiniz." );
-        } else {
-            System.out.println("Burcunuz " + burc);
+        else {
+            System.out.println("Geçerli bir ay giriniz..");
+        }
+
+        if(isError == true){
+            System.out.println("Geçerli bir gün giriniz..");
+        }
+        else{
+            System.out.println("Burcunuz: "+burc);
         }
     }
 }
