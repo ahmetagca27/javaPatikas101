171b6d9c7e1df742d7c50aa25051a865486e0fc8
Ahmet AGCA <113193339+ahmetagca27@users.noreply.github.com>
2023-05-24 17:56:55 +0300

    basamakSayiToplami.java

--- src/basamakSayiToplami/basamakSayiToplami.java
+++ src/basamakSayiToplami/basamakSayiToplami.java
@@ -1,18 +1,18 @@
 package basamakSayiToplami;
 import java.util.Scanner;
+
 public class basamakSayiToplami {
     public static void main(String[] args) {
-        int sayi , total = 0, counter=0,tempsayi=0,bas;
+        int sayi , total = 0, tempsayi = 0;
         Scanner input = new Scanner(System.in);
+        
         System.out.print("Lütfen Bir sayı Girin: ");
         sayi = input.nextInt();
         tempsayi=sayi;
         while (tempsayi != 0) {
             total += tempsayi % 10;
             tempsayi /= 10;
-            counter++;
         }
         System.out.println("Girilen Sayının Sayı değerleri Toplamı.: " + total);
-
     }
 }
