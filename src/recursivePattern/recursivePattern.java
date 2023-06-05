26ba1e1ee01fedb01062d800bf6fbb8cc0f4d0ce
ahmet.agca <ahmet.agca@yahoo.com>
2023-06-05 22:05:56 +0300

    recursivePattern.java

--- src/recursivePattern/recursivePattern.java
+++ src/recursivePattern/recursivePattern.java
@@ -6,14 +6,14 @@ public class recursivePattern {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number to find pattern: ");
         int num = input.nextInt();
-        PatternCreator(num, num, -1);
+        patternCreator(num, num, -1);
     }
 
-    static void PatternCreator(int constantNum, int variableNum, int sign)
+    static void patternCreator(int constantNum, int variableNum, int sign)
     {
         if(variableNum > constantNum) return;
         if(variableNum < 1) sign *= -1;
         System.out.print(" " + variableNum + " ");
-        PatternCreator(constantNum, variableNum  + sign * 5, sign);
+        patternCreator(constantNum, variableNum  + sign * 5, sign);
     }
 }
